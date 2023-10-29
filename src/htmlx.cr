6295cb7ea8a91d6d5fd61badd3d8da91aebaf21d
Billy.Zheng <vil963@gmail.com>
2023-10-29 14:01:20 +0800

    Refine code.

--- src/htmlx.cr
+++ src/htmlx.cr
@@ -2,12 +2,93 @@ require "kemal"
 require "kemal-flash"
 require "hashr"
 
+module RandomName
+  extend self
+
+  VERSION = "0.1.0"
+
+  ALPHABET = "abcdefghijkmnopqrstuvwxyz".chars
+
+  def random(chars : Int32)
+    String.build(chars) do |sb|
+      chars.times { sb << ALPHABET.sample }
+    end
+  end
+end
+
+module RandomNumber
+  extend self
+
+  VERSION = "0.1.0"
+
+  ALPHABET = "0123456789".chars
+
+  def random(chars : Int32)
+    String.build(chars) do |sb|
+      chars.times { sb << ALPHABET.sample }
+    end
+  end
+end
+
+class Archiver
+  # TODO: archive_status 改为 enum
+  property archive_status = "Waiting"
+
+  property archive_progress = 0
+
+  # 状态有三种，等待，运行中，完成
+  def status
+    archive_status
+  end
+
+  def progress
+    archive_progress
+  end
+
+  def run
+    if archive_status == "Waiting"
+      self.archive_status = "Running"
+      self.archive_progress = 0
+      # self.archive.thread = nil
+      spawn run_impl
+    end
+  end
+
+  def run_impl
+    10.times do |i|
+      sleep rand
+
+      return if archive_status != "Running"
+
+      self.archive_progress = (i + 1)/10.0
+    end
+
+    sleep 1
+
+    return if archive_status != "Running"
+
+    self.archive_status = "Complete"
+  end
+
+  def archive_file
+    "contacts.json"
+  end
+
+  def reset
+    self.archive_status = "Waiting"
+  end
+end
+
 Kemal::Session.config do |config|
   config.secret = "my_super_secret"
 end
 
 class Contact
   property id : Int32, first : String?, last : String?, phone : String?, email : String?, errors = {} of String => String
+
+  # def self.random_name(string_length)
+  #   rand(36**string_length).to_s(36)
+  # end
 
   def initialize(@first, @last, @phone, @email, @errors)
     @id = 1
@@ -27,172 +108,115 @@ class Contact
 
   def self.all(page)
     contacts =
-      if page == 1
-        [
+      case page
+      when 1
+        (1..10).map do |i|
+          rand_name = RandomName.random(5) + RandomNumber.random(3)
           {
-            id:     1,
-            first:  "billy",
+            id:     i,
+            first:  rand_name,
             last:   "zheng",
-            phone:  "18620053121",
-            email:  "vil963@gmail.com",
+            phone:  "1862005#{rand(5000..9999)}",
+            email:  "#{rand_name}@gmail.com",
             errors: {} of String => String,
-          },
+          }
+        end
+      when 2
+        (11..20).map do |i|
+          rand_name = RandomName.random(5) + RandomNumber.random(3)
           {
-            id:     2,
-            first:  "xuan",
+            id:     i,
+            first:  rand_name,
             last:   "zheng",
-            phone:  "13603579818",
-            email:  "retired@qq.com",
+            phone:  "1862005#{rand(5000..9999)}",
+            email:  "#{rand_name}@gmail.com",
             errors: {} of String => String,
-          },
+          }
+        end
+      when 3
+        (21..30).map do |i|
+          rand_name = RandomName.random(5) + RandomNumber.random(3)
           {
-            id:     3,
-            first:  "juan",
+            id:     i,
+            first:  rand_name,
             last:   "zheng",
-            phone:  "15026846909",
-            email:  "wj760527@163",
-            errors: {"email" => "error"},
-          },
-          {
-            id:     4,
-            first:  "billy",
-            last:   "zheng",
-            phone:  "18620053121",
-            email:  "vil963@gmail.com",
+            phone:  "1862005#{rand(5000..9999)}",
+            email:  "#{rand_name}@gmail.com",
             errors: {} of String => String,
-          },
+          }
+        end
+      when 4
+        (31..40).map do |i|
+          rand_name = RandomName.random(5) + RandomNumber.random(3)
           {
-            id:     5,
-            first:  "xuan",
+            id:     i,
+            first:  rand_name,
             last:   "zheng",
-            phone:  "13603579818",
-            email:  "retired@qq.com",
+            phone:  "1862005#{rand(5000..9999)}",
+            email:  "#{rand_name}@gmail.com",
             errors: {} of String => String,
-          },
+          }
+        end
+      when 5
+        (41..50).map do |i|
+          rand_name = RandomName.random(5) + RandomNumber.random(3)
           {
-            id:     6,
-            first:  "juan",
+            id:     i,
+            first:  rand_name,
             last:   "zheng",
-            phone:  "15026846909",
-            email:  "wj760527@163",
-            errors: {"email" => "error"},
-          },
-          {
-            id:     7,
-            first:  "billy",
-            last:   "zheng",
-            phone:  "18620053121",
-            email:  "vil963@gmail.com",
+            phone:  "1862005#{rand(5000..9999)}",
+            email:  "#{rand_name}@gmail.com",
             errors: {} of String => String,
-          },
+          }
+        end
+      when 6
+        (51..60).map do |i|
+          rand_name = RandomName.random(5) + RandomNumber.random(3)
           {
-            id:     8,
-            first:  "xuan",
+            id:     i,
+            first:  rand_name,
             last:   "zheng",
-            phone:  "13603579818",
-            email:  "retired@qq.com",
+            phone:  "1862005#{rand(5000..9999)}",
+            email:  "#{rand_name}@gmail.com",
             errors: {} of String => String,
-          },
+          }
+        end
+      when 7
+        (61..70).map do |i|
+          rand_name = RandomName.random(5) + RandomNumber.random(3)
           {
-            id:     9,
-            first:  "juan",
+            id:     i,
+            first:  rand_name,
             last:   "zheng",
-            phone:  "15026846909",
-            email:  "wj760527@163",
-            errors: {"email" => "error"},
-          },
-          {
-            id:     10,
-            first:  "juan",
-            last:   "zheng",
-            phone:  "15026846909",
-            email:  "wj760527@163",
-            errors: {"email" => "error"},
-          },
-        ]
-      elsif page == 2
-        [
-          {
-            id:     11,
-            first:  "juan",
-            last:   "zheng",
-            phone:  "18620053121",
-            email:  "vil963@gmail.com",
+            phone:  "1862005#{rand(5000..9999)}",
+            email:  "#{rand_name}@gmail.com",
             errors: {} of String => String,
-          },
+          }
+        end
+      when 8
+        (71..80).map do |i|
+          rand_name = RandomName.random(5) + RandomNumber.random(3)
           {
-            id:     12,
-            first:  "wei",
+            id:     i,
+            first:  rand_name,
             last:   "zheng",
-            phone:  "13603579818",
-            email:  "retired@qq.com",
+            phone:  "1862005#{rand(5000..9999)}",
+            email:  "#{rand_name}@gmail.com",
             errors: {} of String => String,
-          },
+          }
+        end
+      when 9
+        (81..90).map do |i|
+          rand_name = RandomName.random(5) + RandomNumber.random(3)
           {
-            id:     13,
-            first:  "retired",
+            id:     i,
+            first:  rand_name,
             last:   "zheng",
-            phone:  "15026846909",
-            email:  "wj760527@163",
-            errors: {"email" => "error"},
-          },
-          {
-            id:     14,
-            first:  "billy",
-            last:   "zheng",
-            phone:  "18620053121",
-            email:  "vil963@gmail.com",
+            phone:  "1862005#{rand(5000..9999)}",
+            email:  "#{rand_name}@gmail.com",
             errors: {} of String => String,
-          },
-          {
-            id:     15,
-            first:  "Charles",
-            last:   "zheng",
-            phone:  "13603579818",
-            email:  "retired@qq.com",
-            errors: {} of String => String,
-          },
-          {
-            id:     16,
-            first:  "billy",
-            last:   "zheng",
-            phone:  "15026846909",
-            email:  "wj760527@163",
-            errors: {"email" => "error"},
-          },
-          {
-            id:     17,
-            first:  "juan",
-            last:   "zheng",
-            phone:  "18620053121",
-            email:  "vil963@gmail.com",
-            errors: {} of String => String,
-          },
-          {
-            id:     18,
-            first:  "Mike",
-            last:   "zheng",
-            phone:  "13603579818",
-            email:  "retired@qq.com",
-            errors: {} of String => String,
-          },
-          {
-            id:     19,
-            first:  "billy",
-            last:   "zheng",
-            phone:  "15026846909",
-            email:  "wj760527@163",
-            errors: {"email" => "error"},
-          },
-          {
-            id:     20,
-            first:  "tom",
-            last:   "zheng",
-            phone:  "15026846909",
-            email:  "wj760527@163",
-            errors: {"email" => "error"},
-          },
-        ]
+          }
+        end
       else
         [
           {
